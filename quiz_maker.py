a98c70707749f39edd86b9d149ed6134004174d6
mob99-9 <jamesorozco20050@gmail.com>
2025-04-07 17:18:42 +0800

    testing file handling skills after tutorial

--- quiz_maker.py
+++ quiz_maker.py
@@ -2,6 +2,18 @@
 
 # make a text file 
 
+file_name = "quiz.txt"
+
+# adds title on the quiz file
+
+with open(file_name, "w") as file:
+    file.write("Quiz Maker")
+
 # make a function for asking questions and answers
+text = open("quiz.txt", "r")
+
+print(text.read())
+
+text.close()
 
 # save the text file 
