f27714e80b57875819160ddc513cec85c8d5b2b3
mob99-9 <jamesorozco20050@gmail.com>
2025-04-09 14:54:19 +0800

    finish program and testing multiple times

--- quiz_maker.py
+++ quiz_maker.py
@@ -1,19 +1,36 @@
 # make a text file with questions and possible answers from the user
 
 # make a text file 
-
+running = True
 file_name = "quiz.txt"
 
 # adds title on the quiz file
 
 with open(file_name, "w") as file:
-    file.write("Quiz Maker")
+    file.write("Quiz File\n\n")
 
-# make a function for asking questions and answers
-text = open("quiz.txt", "r")
+# make a function for adding questions and answers to the text file
 
-print(text.read())
+def add_to_file(user_input):
+    with open(file_name, "a") as text_file:
+        text_file.write(f"{user_input}\n")
 
-text.close()
+# ask user for a question, 4 possible answer (a, b, c, d) and the right answer (A), infinitely until saying exit
 
-# save the text file 
+choices = ["a", "b", "c", "d"]
+
+while running == True:
+    user_question = input("Enter a question (type 'exit' to exit): ")
+    if user_question == "exit":
+        running = False
+    else:
+        add_to_file(user_question.capitalize())
+
+        for letter in choices:
+            user_choices = input(f"{letter}. Enter a possible answer: ")
+            add_to_file(f"{letter}. {user_choices}")
+
+        user_answer = input("Enter the letter of the right answer: ")
+        add_to_file(f"Answer: {user_answer.capitalize()}")
+
+# save the text file (It is auto save and auto reset every program run)
